b4ccfc0efafb4addc82e2cdb979aabb6d3055188
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2024-08-01 11:28:21 +0200

    added ai api intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-function generatePoem(event){
-    event.preventDefault()
-
+function displayPoem(response){
     new Typewriter('#poem', {
-     strings: ' a language can be influenced by multiple approaches.',
+     strings: response.data.answer,
      autoStart: true,
      delay:1,
      cursor:"",
    });
+
+}
+
+
+function generatePoem(event){
+    event.preventDefault();
+
+    let instrunctionInput = document.querySelector("#user-instructions")
+
+
+
+    let apiKey ='0cb1t3949f347fc75ae8b7o36a1c0c57';
+    let prompt =`User instructions: Generate an Italian poem about ${instrunctionInput.value} `;
+    let context ="You are a very romantic and polite poem expert, please generate a 4 line poem in basic html and seperate each line with a <br /> and listen to the user's instructions.Do not include a title.";
+
+
+
+    let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+
+    console.log("Generating poem")
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context:${context}`);
+
+    axios.get(apiUrl).then(displayPoem)
+
 };
 
 let poemFormElement = document.querySelector("#poem-generator")
