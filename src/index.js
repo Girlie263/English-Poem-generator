d5e94b8dd0733018faa00904b8ddf9c3d80e9d5f
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2024-08-01 13:43:10 +0200

    improved user experience

--- src/index.js
+++ src/index.js
@@ -19,10 +19,12 @@ function generatePoem(event){
     let apiKey ='0cb1t3949f347fc75ae8b7o36a1c0c57';
     let prompt =`User instructions: Generate an Italian poem about ${instrunctionInput.value} `;
     let context ="You are a very romantic and polite poem expert, please generate a 4 line poem in basic html and seperate each line with a <br /> and listen to the user's instructions.Do not include a title.";
-
-
-
     let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `Generating an Italian poem about ${instrunctionInput.value}...`
 
 
     console.log("Generating poem")
